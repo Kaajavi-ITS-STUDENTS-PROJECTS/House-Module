ddba39e19d366a9aa0b7b9bc4bd71258deb245df
RodrigoToledoA <rodry.toledo@gmail.com>
2015-08-24 11:33:08 -0300

    Agregamos la inicializacion de los pines

--- asd.py
+++ asd.py
@@ -2,10 +2,14 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-
+# init list with pin numbers
+pinList = [15, 22, 9, 10]
 # loop through pins and set mode and state to 'low'
+for i in pinList:
+    GPIO.setup(i, GPIO.OUT)
+    #GPIO.output(i, GPIO.LOW)
 SleepTimeL = 2
-    # main loop
+# main loop
 try:
     GPIO.output(15, GPIO.LOW)
     print "ONE"
