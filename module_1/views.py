79c7b1f407dfc8b08e06a73762bf34bf9dec961c
frandemo <franciscodemaussion@gmail.com>
2015-08-24 20:25:43 -0300

    Eliminar partes del relay de las views

--- module_1/views.py
+++ module_1/views.py
@@ -1,32 +1,11 @@
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-import RPi.GPIO as GPIO
-import time
-PRENDIDO = GPIO.LOW
-APAGADO = GPIO.HIGH
-
-# init de los pins
-GPIO.setmode(GPIO.BCM)
-pinList = [15, 22, 9, 10]
-for i in pinList:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, APAGADO)
-
-# Create your views here.
 
 def index(request):
     context = RequestContext(request)
     return render_to_response('index.html',
                               context)
-prendido = False
 
 def abrirPuerta(request):
-    if prendido==False:
-        GPIO.output(15, PRENDIDO)
-        prendido = True
-    else:
-        GPIO.output(15, APAGADO)
-        prendido = False
-
     return render_to_response('index.html',
                              context)
