7e112dc24271f1fdcced354361b9968b0b82f60f
RodrigoToledoA <rodry.toledo@gmail.com>
2015-08-24 12:06:47 -0300

    Boton abrir puerta

--- module_1/urls.py
+++ module_1/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$', 'module_1.views.index', name='index'),
+                       url(r'^$', 'module_1.views.abrirPuerta', name='index'),
                        )
