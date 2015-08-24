0d18d652c1daa34969df86b8d44da4d4c4c2e0b2
RodrigoToledoA <rodry.toledo@gmail.com>
2015-08-24 09:42:30 -0300

    Probar relay

---
+++ asd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+# init list with pin numbers
+pinList = [12, 11, 13, 16]
+# loop through pins and set mode and state to 'low'
+for i in pinList:
+GPIO.setup(i, GPIO.OUT)
+GPIO.output(i, GPIO.HIGH)
+# time to sleep between operations in the main loop
+SleepTimeL = 2
+# main loop
+try:
+GPIO.output(12, GPIO.LOW)
+print "ONE"
+time.sleep(SleepTimeL);
+GPIO.output(11, GPIO.LOW)
+print "TWO"
+time.sleep(SleepTimeL);
+GPIO.output(13, GPIO.LOW)
+print "THREE"
+time.sleep(SleepTimeL);
+GPIO.output(16, GPIO.LOW)
+print "FOUR"
+time.sleep(SleepTimeL);
+GPIO.cleanup()
+print "Good bye!"
+# End program cleanly with keyboard
+except KeyboardInterrupt:
+print " Quit"
+# Reset GPIO settings
+GPIO.cleanup()
