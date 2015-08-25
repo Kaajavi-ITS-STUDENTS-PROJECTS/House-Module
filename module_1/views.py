37165cb605b73b86c449a79f6a83ba0945b60bd5
frandemo <franciscodemaussion@gmail.com>
2015-08-24 21:07:10 -0300

    Arreglo view implement relay_functions.py

--- module_1/views.py
+++ module_1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
+import relay_functions
 
 def index(request):
     context = RequestContext(request)
@@ -7,5 +8,6 @@ def index(request):
                               context)
 
 def abrirPuerta(request):
+    relay_functions.puerta("open")
     return render_to_response('index.html',
                              context)
