3e8ae6493eea267dbb31347e3b250821af44e434
RodrigoToledoA <rodry.toledo@gmail.com>
2015-09-07 10:20:25 -0300

    Prueba relay abrir y get status paralelo

--- module_1/relay_functions.py
+++ module_1/relay_functions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 # reinvertir la programacion
 ON = GPIO.LOW
@@ -12,16 +12,12 @@ pinList = [15, 22, 9, 10]
 for i in pinList:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, OFF)
-    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def relay(do, pin):
-    if do=="open":
-        GPIO.output(pin, ON)
-    else:
-        GPIO.output(pin, OFF)
+    try:
+        if do=="open":
+            GPIO.output(pin, ON)
+        sleep(3)
 
-def getStatus(pin):
-    if GPIO.input(pin):
-        return True
-    else:
-        return False
+    except:
+        GPIO.cleanup()
---
+++ module_1/relay_status.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+pinList = [15, 22, 9, 10]
+
+for i in pinList:
+    GPIO.setup(i, GPIO.IN)
+
+def getStatus(pin):
+    if GPIO.input(pin):
+        return True
+    else:
+        return False
