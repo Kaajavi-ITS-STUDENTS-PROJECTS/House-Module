396019b12f986f60f75f128565b82a56bea66663
frandemo <franciscodemaussion@gmail.com>
2015-09-07 10:36:49 -0300

    Prueba relays

--- module_1/relay_functions.py
+++ module_1/relay_functions.py
@@ -10,14 +10,18 @@ OFF = GPIO.HIGH
 GPIO.setmode(GPIO.BCM)
 pinList = [15, 22, 9, 10]
 for i in pinList:
+    GPIO.setup(i, GPIO.IN)
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, OFF)
 
 def relay(do, pin):
-    try:
-        if do=="open":
-            GPIO.output(pin, ON)
-        sleep(3)
+    if do=="open":
+        GPIO.output(pin, ON)
+    else:
+        GPIO.output(pin, OFF)
 
-    except:
-        GPIO.cleanup()
+def getStatus(pin):
+    if GPIO.input(pin):
+        return True
+    else:
+        return False
