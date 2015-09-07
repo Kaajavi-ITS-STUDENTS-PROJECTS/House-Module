865c0aa23c9db5fee3406d7ee489129f428c8013
Ubuntu User <ulises.torrella@gmail.com>
2015-09-07 10:49:21 -0300

    Uni los cambios de onoff con module_1

--- module_1/views.py
+++ module_1/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 import relay_functions
-from module_1.models import Luz, Puerta, Habitacion, Sanitario, Alarma
-import time
+from onoff.models import Luz, Puerta, Habitacion, Sanitario, Alarma, Usuario
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import Userimport time
 # Create your views here.
 
 def index(request):
@@ -49,15 +50,19 @@ def index(request):
 def luz(request, id_luz):
     context = RequestContext(request)
 ## Codigo para que prenda y apage la luz
-##
-    luz = Luz.objects.get(id = id_luz)
-    if luz.status:
-        relay_functions.relay("close",luz.pin)
-        luz.status = False
-    else:
-        relay_functions.relay("open",luz.pin)
-        luz.status = True
-    luz.save()
+##  print "ahi viene" 
+    lista_permitidos = Usuario.objects.filter(permisos_luces=id_luz)
+    if lista_permitidos.__str__() != "[]":
+        for permitido in lista_permitidos:
+            if permitido.user.id == request.user.id:
+                luz = Luz.objects.get(id = id_luz)
+                if luz.status:
+                    relay_functions.relay("close",luz.pin)
+                    luz.status = False
+                else:
+                    relay_functions.relay("open",luz.pin)
+                    luz.status = True
+                luz.save()
     luces = Luz.objects.all()
     puertas = Puerta.objects.all()
     habitaciones = Habitacion.objects.all()
@@ -68,14 +73,19 @@ def luz(request, id_luz):
 def puerta(request, id_puerta):
     context = RequestContext(request)
     puerta = Puerta.objects.get(id = id_puerta)
-    if puerta.status:
-        relay_functions.relay("close",puerta.pin)
-        puerta.status = False
-    else:
-        relay_functions.relay("open",puerta.pin)
-        puerta.status = True
-    puerta.save()
-    time.sleep(10)
+    lista_permitidos = Usuario.objects.filter(permisos_puertas=id_puerta)
+    if lista_permitidos.__str__() != "[]":
+        for permitido in lista_permitidos:
+            if permitido.user.id == request.user.id:
+                print "TRUEEE"
+                if puerta.status:
+                    relay_functions.relay("close",puerta.pin)
+                    puerta.status = False
+                else:
+                    relay_functions.relay("open",puerta.pin)
+                    puerta.status = True
+                puerta.save()
+                time.sleep(10)
     puerta = Puerta.objects.get(id = id_puerta)
     if puerta.status:
         ## Codigo para que cierra la puerta
@@ -112,14 +122,17 @@ def sanitario(request, id_sanitario):
 def habitacion(request, id_habitacion):
     context = RequestContext(request)
     ##
-    habitacion = Habitacion.objects.get(id = id_habitacion)
-    if habitacion.status:
-        habitacion.status = False
-    else:
-        habitacion.status = True
+    lista_permitidos = Usuario.objects.filter(permisos_habitaciones=id_habitacion)
+    if lista_permitidos.__str__() != "[]":
+        for permitido in lista_permitidos:
+            if permitido.user.id == request.user.id:
+                print "TRUEEE"
+                habitacion = Habitacion.objects.get(id = id_habitacion)
+                if habitacion.status:
+                    habitacion.status = False
+                else:
+                    habitacion.status = True
     luces = Luz.objects.filter(lugar_id = id_habitacion)
-
-
     for luz in luces:
         ##Codigo para que apage cada una de las luces del for
         luz.status = habitacion.status
