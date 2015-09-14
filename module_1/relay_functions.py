c7c0012647b43df88f74906ca7af8351113f6648
frandemo <franciscodemaussion@gmail.com>
2015-09-14 12:00:39 -0300

    Chau array

--- module_1/relay_functions.py
+++ module_1/relay_functions.py
@@ -8,19 +8,18 @@ OFF = GPIO.HIGH
 # init de los pins
 GPIO.setmode(GPIO.BCM)
 pinList = [15, 22, 9, 10]
-for i in pinList:
-    GPIO.setup(i, GPIO.IN)
-    GPIO.input(i)
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, OFF)
+GPIO.setup(pinList, GPIO.OUT, initial=OFF)
+GPIO.setup(pinList, GPIO.IN)
 
 def relay(do, pin):
+    GPIO.setup(pinList, GPIO.OUT, initial=OFF)
     if do=="open":
         GPIO.output(pin, ON)
     else:
         GPIO.output(pin, OFF)
 
 def getStatus(pin):
+    GPIO.setup(pinList, GPIO.IN)
     if GPIO.input(pin):
         return True
     else:
