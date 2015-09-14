a5cae9b5600eb1c159fd73f8a56820b1312a50cf
frandemo <franciscodemaussion@gmail.com>
2015-09-14 09:03:15 -0300

    si

--- module_1/relay_functions.py
+++ module_1/relay_functions.py
@@ -10,8 +10,8 @@ OFF = GPIO.HIGH
 GPIO.setmode(GPIO.BCM)
 pinList = [15, 22, 9, 10]
 for i in pinList:
-    GPIO.setup(i, GPIO.IN)
     GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, OFF)
 
 def relay(do, pin):
     if do=="open":
