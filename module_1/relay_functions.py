07a6402cabbf76c2acd0f48bf346a2d970f97973
frandemo <franciscodemaussion@gmail.com>
2015-09-14 11:47:49 -0300

    Arreglos

--- module_1/relay_functions.py
+++ module_1/relay_functions.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-from time import sleep
 
 # reinvertir la programacion
 ON = GPIO.LOW
 OFF = GPIO.HIGH
 
 # init de los pins
-GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 pinList = [15, 22, 9, 10]
 for i in pinList:
     GPIO.setup(i, GPIO.IN)
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, OFF)
+    GPIO.input(i)
 
 def relay(do, pin):
     if do=="open":
