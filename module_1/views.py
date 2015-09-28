d70e10935484db6b3a211e7a972555c90d240931
RodrigoToledoA <rodry.toledo@gmail.com>
2015-09-28 08:30:16 -0300

    Prueba websocket

--- module_1/views.py
+++ module_1/views.py
@@ -209,3 +209,15 @@ def logout_user(request):
     context = RequestContext(request)
     return redirect('/login')
 
+from omnibus.api import publish
+
+def send_hello_world():
+    publish(
+        'mychannel',  # the name of the channel
+        'hello',  # the `type` of the message/event, clients use this name
+                  # to register event handlers
+        {'text': 'Hello world'},  # payload of the event, needs to be
+                                  # a dict which is JSON dumpable.
+        sender='server'  # sender id of the event, can be None.
+    )
+    return print publish
