271c5088668944545714feca312f617e639d2b26
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-08 14:25:29 -0300

    limpiando imports

--- HouseModule/settings.py
+++ HouseModule/settings.py
@@ -14,8 +14,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-from .celery import app as celery_app
-
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
