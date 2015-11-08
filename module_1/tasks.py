9fdf49e0455a9f4e70f4f6124e7b4416a701be0f
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-08 12:08:29 -0300

    Tasks.py ejemplo de funcion periodica

--- module_1/tasks.py
+++ module_1/tasks.py
@@ -1,8 +1,13 @@
-from __future__ import absolute_import
-
-from celery import shared_task
-
-#@shared_task para que no dependa de un proyecto completo para andar sino que la app puede hacer las funciones en cualquier lado que este celery
-@shared_task
-def add(x, y):
-    return x + y
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from myapp.utils import scrapers
+from celery.utils.log import get_task_logger
+from datetime import datetime
+logger = get_task_logger(__name__)
+# A periodic task that will run every minute (the symbol "*" means every)
+@periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="*")))
+def scraper_example():
+    logger.info("Start task")
+    now = datetime.now()
+    result = scrapers.scraper_example(now.day, now.minute)
+    logger.info("Task finished: result = %i" % result)
