5c05dae951a3498b2ce0f73ed16a0976f33988b6
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-08 15:19:58 -0300

    Probando tareas preiodicas

--- module_1/tasks.py
+++ module_1/tasks.py
@@ -5,6 +5,6 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 # A periodic task that will run every minute (the symbol "*" means every)
 
-@periodic_task(run_every=(crontab(hour="8", minute="*", day_of_week="mon")))
+@periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="sun")))
 def scraper_example():
     print("This is run every Monday morning at 8 every minute")
