d89fd6545699afb110d35e8abc629f683f20182a
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-08 12:47:55 -0300

    Limpiando imports de mas

--- module_1/tasks.py
+++ module_1/tasks.py
@@ -1,13 +1,10 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
-from myapp.utils import scrapers
 from celery.utils.log import get_task_logger
-from datetime import datetime
+
 logger = get_task_logger(__name__)
 # A periodic task that will run every minute (the symbol "*" means every)
-@periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="*")))
+
+@periodic_task(run_every=(crontab(hour="8", minute="*", day_of_week="mon")))
 def scraper_example():
-    logger.info("Start task")
-    now = datetime.now()
-    result = scrapers.scraper_example(now.day, now.minute)
-    logger.info("Task finished: result = %i" % result)
+    print("This is run every Monday morning at 8 every minute")
