170f8f774010b53269109b3867002526de5434d8
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-09 11:57:36 -0300

    probando

--- module_1/tasks.py
+++ module_1/tasks.py
@@ -2,10 +2,10 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task, task
 from celery.utils.log import get_task_logger
 import relay_functions
-
+relay_functions.setting_pines()
 @task
-def on():
-    relay_functions.relay("open" ,15)
+def on(pin):
+    relay_functions.relay("open" ,pin)
 
 #@task
 #def off(pin):
