963cd56baba7fe1c8e8df904611eb1715005eea3
frandemo <franciscodemaussion@gmail.com>
2015-11-09 10:24:52 -0300

    agregar regla

--- module_1/views.py
+++ module_1/views.py
@@ -311,8 +311,7 @@ def add_rule(request):
 
 def del_rule(request):
     context = RequestContext(request)
-    print request.POST['id_r']
-    #rule = Regla.objects.get(id = )
+    rule = Regla.objects.get(id = request.POST['id_r'])
     rule.delete()
     rules = Regla.objects.all()
     return render_to_response('tab.html',{'rules':rules},context)
