1ba0414fd8faa68a41f55cf7eb472a86eecf6ee5
Ulises Torrella <ulises.torrella@gmail.com>
2015-11-13 11:20:53 -0300

    test invertir logs

--- module_1/views.py
+++ module_1/views.py
@@ -405,8 +405,7 @@ def get_current_user(request):
 def logs(request):
     context = RequestContext(request)
     logs = Log.objects.all()
-    for log in logs:
-        print "anda"
+    logs = [::-1]
 
     return render_to_response('logs.html',{'logs':logs},context)
 
