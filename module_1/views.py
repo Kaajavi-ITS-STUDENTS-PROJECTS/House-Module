294ea94ceccca35c2ca5ab3595615425bbf22dde
Ulises Torrella <ulises.torrella@gmail.com>
2015-11-13 08:27:16 -0300

    --

--- module_1/views.py
+++ module_1/views.py
@@ -385,7 +385,7 @@ def get_current_user(request):
     else:
         return render_to_response('perfil.html',{'username':""},context)
 
-def get_current_user(request):
+def logs(request):
     context = RequestContext(request)
     return render_to_response('logtable.html',{},context)
 
