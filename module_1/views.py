bd49d278b961b36a06bc4bc6986c2ed37be45537
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-13 11:42:18 -0300

    Limpio autoclose viwe

--- module_1/views.py
+++ module_1/views.py
@@ -46,15 +46,6 @@ def index(request):
         context = RequestContext(request)
         return render_to_response('login.html',
                               context)
-
-
-def recargar(helper, result):
-    publish(
-        'module_recargar',
-        'reload',
-        sender='server'  # sender id of the event, can be None.
-    )
-
 def luz(request):
     context = RequestContext(request)
     id= request.POST.get('id')
@@ -102,23 +93,6 @@ def puerta(request):
                     setPuerta(True, puerta)
                 puerta.save()
                 print puerta.status
-    if lista_permitidos.__str__() != "[]":
-        for permitido in lista_permitidos:
-            if permitido.user.id == request.user.id:
-                if puerta.auto_close:
-                    print "helper"
-                    helper = "#puerta-" + str(puerta.id)
-                    print "recargar"
-                    recargar(helper, render_to_response('puertas.html',{'puerta':puerta}, context))
-                    print "time sleep"
-                    time.sleep(5)
-                    print "cerrar puerta"
-                    setPuerta(False,puerta)
-                    print "guarda status"
-                    puerta.status = False
-                    print "save"
-                    puerta.save()
-    print "Fuera del if"
     puertas = Puerta.objects.all()
     return render_to_response('puertas.html',{'puerta':puerta}, context)
 
