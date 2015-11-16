9ff3a7502bc33546c0ebeb61d6994e778596ba1d
Ulises Torrella <ulises.torrella@gmail.com>
2015-11-16 08:47:27 -0300

    arreglando (creo)

--- module_1/views.py
+++ module_1/views.py
@@ -443,7 +443,7 @@ def logs(request):
     cont = 0
     print len(logs)
     for log in logs:
-        fechas.append(cont) = cont
+        fechas.append(cont) = cont.fecha
         cont+=1
     return render_to_response('logs.html',{'logs':logs,'fechas':fechas},context)
 
