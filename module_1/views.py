7114e8b599a40d9a2c08cb11c7dd8f403bcbf8de
Ulises Torrella <ulises.torrella@gmail.com>
2015-11-16 11:28:36 -0300

    debug print

--- module_1/views.py
+++ module_1/views.py
@@ -446,9 +446,9 @@ def logs(request):
 
 def filterlog(request):
     context = RequestContext(request)
-    print request.GET['day']
     logs = Log.objects.filter(fecha=request.GET['day'])
     logs = logs[::-1]
+    print logs
     return render_to_response('logtable.html',{'logs':logs},context)
 
 
