0614d2556648ef6c95388173d6fa3c9db17d801a
RodrigoToledoA <rodry.toledo@gmail.com>
2015-11-20 08:35:57 -0300

    --------------

--- module_1/views.py
+++ module_1/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 import relay_functions
-import asdasd
 from module_1.models import Luz, Puerta, Habitacion, Sanitario, Alarma, Usuario, Regla, Log
 from django.contrib.auth import authenticate
 import time
@@ -121,7 +120,7 @@ def puerta(request):
     return response
 
 def superSecret(request):
-    asdasd.dance()
+    relay_functions.dance()
     return "DANCE!!"
 
 
